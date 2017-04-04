fb0fb4bb7b3c852864a9c9d0b60e4744f7f33380
John Westhoff <jwesthof@nd.edu>
2017-04-04 01:11:40 -0400

    Worked more on front end

--- frontend/www/js/index.js
+++ frontend/www/js/index.js
@@ -1,10 +1,10 @@
-
 document.addEventListener("deviceready", onDeviceReady, false);
 var gScore = 0;
 var gCurPage = '';
 var gFriendNum = 0;
-
+var gChalNum = 0;
 var gSelFriend = '';
+var gUser = '';
 
 function onLoad()
 {
@@ -19,6 +19,9 @@ function onPageLoad() {
         case 'friendpage':
             populateFriends();
         break;
+        case 'mainpage':
+            populateChallenges();
+        break;
     }
 }
 
@@ -27,17 +30,115 @@ function onfriend(e) {
     jump('userinfopage');
 }
 
+function populateChallenges() {
+    var formData={
+        "user": gUser,
+    };
+    $.post({
+        url: 'http://johnwesthoff.com:3111/challenges',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        processData: false,
+        success: function(responseData, textStatus, jqXHR) {
+            console.log(responseData);
+            if (responseData.status) {
+                $('#clist').empty();
+                var challenges = responseData.challenges;
+                gChalNum = 0;
+                if (challenges) {
+                    for (var i = 0; i < challenges.length; i += 1) {
+                        var name = "";
+                        if (challenges[i]['foe'] === gUser) {
+                            name = challenges[i]['user'];
+                        } else {
+                            name = challenges[i]['foe'];
+                        }
+                        var newfriend='<div class=\'friend\'\
+                                       id=\'chal'+gChalNum+'\'\
+                                       onclick=\'onfriend(event);\'\
+                                       >'+name+'</div>';
+                        gChalNum += 1;
+                        $('#clist').append(newfriend);
+                    } 
+                }
+            }
+        },
+        error: function (responseData, textStatus, errorThrown) {
+        }
+    });
+}
 function populateFriends() {
-    $('#friendlist').empty();
-    gFriendNum = 0;
-    for (var i = 0; i < 10; i += 1) {
-    var newfriend='<div class=\'friend\'\
-                   id=\'friend'+gFriendNum+'\'\
-                   onclick=\'onfriend(event);\'\
-                   >FRIEND '+(gFriendNum+1)+'</div>';
-    gFriendNum += 1;
-    $('#friendlist').append(newfriend);
-    } 
+    var formData={
+        "user": gUser,
+    };
+    $.post({
+        url: 'http://johnwesthoff.com:3111/friendlist',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        processData: false,
+        success: function(responseData, textStatus, jqXHR) {
+            if (responseData.status) {
+                $('#friendlist').empty();
+                var friends = responseData.friends;
+                console.log(friends);
+                gFriendNum = 0;
+                for (var i = 0; i < friends.length; i += 1) {
+                var newfriend='<div class=\'friend\'\
+                               id=\'friend'+gFriendNum+'\'\
+                               onclick=\'onfriend(event);\'\
+                               >'+friends[i]['friend']+'</div>';
+                gFriendNum += 1;
+                $('#friendlist').append(newfriend);
+                } 
+            }
+        },
+        error: function (responseData, textStatus, errorThrown) {
+        }
+    });
+}
+
+function addFriend() {
+    var formData={
+        "user": gUser,
+        "friend": $("#af_email").val(),
+    };
+    $.post({
+        url: 'http://johnwesthoff.com:3111/friends',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        processData: false,
+        success: function(responseData, textStatus, jqXHR) {
+            if (responseData.status) {
+                jump("friendpage");
+            } 
+        },
+        error: function (responseData, textStatus, errorThrown) {
+        }
+    });
+}
+
+function issueChallenge() {
+    var formData={
+        "user": gUser,
+        "foe": $("#userheader").html(),
+    };
+    $.post({
+        url: 'http://johnwesthoff.com:3111/newchallenge',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        processData: false,
+        success: function(responseData, textStatus, jqXHR) {
+            if (responseData.status) {
+                jump("matchpage");
+            } 
+        },
+        error: function (responseData, textStatus, errorThrown) {
+        }
+    });
 }
 
 function onDeviceReady()
@@ -65,20 +166,55 @@ function jump(newpage)
     onPageLoad();
 }
 
-function send()
+function login()
 {
-    $.ajax({
-            type: 'POST',
-            url: 'https://johnbot.me/t.php',
-            crossDomain: true,
-            data: '{"some":"json"}',
-            dataType: 'json',
-            success: function(responseData, textStatus, jqXHR) {
-                        alert(responseData);
-                    },
-            error: function (responseData, textStatus, errorThrown) {
-                        alert('POST failed: ' + errorThrown);
-                    }
+    gUser = $("#email").val();
+    var formData={
+        "user": gUser,
+        "pass": $("#password").val(),
+    };
+    $.post({
+        type: 'POST',
+        url: 'http://johnwesthoff.com:3111/login',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        processData: false,
+        success: function(responseData, textStatus, jqXHR) {
+            if (responseData['status']) {
+                jump('mainpage');
+            }
+        },
+        error: function (responseData, textStatus, errorThrown) {
+        }
+    });
+}
+function register()
+{
+    if ($("#reg_password1").val() !== $("#reg_password2").val()) {
+        console.log("DIFFERENT");
+        return;
+    }
+    var formData={
+        "user": $("#reg_email").val(),
+        "pass": $("#reg_password1").val(),
+    };
+    $.post({
+        type: 'POST',
+        url: 'http://johnwesthoff.com:3111/register',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        processData: false,
+        success: function(responseData, textStatus, jqXHR) {
+            if (responseData['status']) {
+                $("#password").val(formData['pass']);
+                $("#email").val(formData['user']);
+                login();
+            }
+        },
+        error: function (responseData, textStatus, errorThrown) {
+        }
     });
 }
 function score(a)
